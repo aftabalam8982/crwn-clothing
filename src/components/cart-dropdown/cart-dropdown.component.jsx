1fe1795c59b0c7c071dca97a856bc41215893c92
aftab-1194 <aftab.alam@classicinformatics.com>
2023-07-11 13:49:33 +0530

    Added cart-item, and modified add to cart logic

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,23 @@
+import { useContext } from "react";
 import Button from "../button/button.component";
 import './cart-dropdown.styles.scss';
+import { cartContext } from "../../context/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 
 
 const CartDropdown = () => {
+
+    const {cartItems} = useContext(cartContext)
+
     return(
         <div className="cart-dropdown-container">
-            <div className="cart-items"/>
+            <div className="cart-items">
+                {
+                    cartItems.map(item =>(
+                        <CartItem key={item.id} cartItem={item} />
+                    ))
+                }
+            </div>
             <Button >go to Checkout</Button>
         </div>
     )

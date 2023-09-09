c1ea0f3ad5dddd508fce6e878cb30d91cb34c1b6
Aftab Alam <aftabalam@Aftabs-MacBook-Air.local>
2023-09-09 12:40:23 +0530

    added link to directory

--- src/components/directory-item/directory-item-component.jsx
+++ src/components/directory-item/directory-item-component.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './directory-item-styles.scss'
+import { Link } from 'react-router-dom'
 
 const DirectoryItem = ({category}) => {
     const {id, imageUrl,  title} = category
   return (
-    <div className="directory-container" key={id}>
+    <div className="directory-item-container" key={id}>
     <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}/>
     <div className="body">
       <h2>{title}</h2>
-      <p>Shop Now</p>
+      <Link to={`shop/${title}`}>Shop Now</Link>
     </div>
   </div>
   )

25678fdb196605d14e1af735ebbf0884ef6c07af
Aftab Alam <aftabalam@Aftabs-MacBook-Air.local>
2023-09-13 18:58:23 +0530

    added useReducer in user.context component'

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 import { CreateUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
@@ -7,16 +7,44 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Invalid user ${type}`);
+  }
+};
+
+const InitialState = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  const [{ currentUser }, dispatch] = useReducer(userReducer, InitialState);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener( (user) => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
-         CreateUserDocumentFromAuth(user);
+        CreateUserDocumentFromAuth(user);
       }
-      setCurrentUser(user)
+      setCurrentUser(user);
     });
     return unsubscribe;
   }, []);

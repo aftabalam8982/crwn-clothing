b2c1147ca43e9c9a5e8ce8e2074815217727d340
Aftab Alam <aftabalam@Aftabs-MacBook-Air.local>
2023-09-13 17:39:40 +0530

    Revert "added useReducer in user.context"
    This reverts commit c9bbb18c7edd133b60aa8e4877f613b57c82e623.

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,54 +1,22 @@
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 import { CreateUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
-import { useReducer } from "react";
 
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 
-const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: "SET_CURRENT_USER",
-};
-
-const userReducer = (state, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return {
-        ...state,
-        currentUser: payload,
-      };
-    default:
-      throw new Error(`Invalid action ${type}`);
-  }
-};
-
-const InitialState = {
-  currentUser: null,
-};
-
 export const UserProvider = ({ children }) => {
-  //const [currentUser, setCurrentUser] = useState(null);
-
-  const [{ currentUser }, dispatch] = useReducer(userReducer, InitialState);
-
-  const setCurrentUser = (user) => {
-    dispatch({
-      type: USER_ACTION_TYPES.SET_CURRENT_USER,
-      payload: user,
-    });
-  };
-
+  const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
+    const unsubscribe = onAuthStateChangedListener( (user) => {
       if (user) {
-        CreateUserDocumentFromAuth(user);
+         CreateUserDocumentFromAuth(user);
       }
-      setCurrentUser(user);
+      setCurrentUser(user)
     });
     return unsubscribe;
   }, []);

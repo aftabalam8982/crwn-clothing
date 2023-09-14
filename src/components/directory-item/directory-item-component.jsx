076feded8d9930bb06f8a27827be7a815981c706
Aftab Alam <aftabf8982@gmail.com>
2023-09-14 23:27:01 +0530

    make changes in item-directory

--- src/components/directory-item/directory-item-component.jsx
+++ src/components/directory-item/directory-item-component.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import './directory-item-styles.scss'
+import React from "react";
+import "./directory-item-styles.scss";
 
-const DirectoryItem = ({category}) => {
-    const {id, imageUrl,  title} = category
+const DirectoryItem = ({ category }) => {
+  const { id, imageUrl, title } = category;
   return (
     <div className="directory-container" key={id}>
-    <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}/>
-    <div className="body">
-      <h2>{title}</h2>
-      <p>Shop Now</p>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default DirectoryItem;

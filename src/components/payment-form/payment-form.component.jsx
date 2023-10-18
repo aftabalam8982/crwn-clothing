41c1e19e59a86c85e22e1f518fa47104e404727f
Aftab <106815509+aftabalam8982@users.noreply.github.com>
2023-10-19 00:18:04 +0530

    @payment feature (#20)
    * deleted the context cauze no longer needed
    
    * added redirect public file
    
    * added payment form component and serverless function to test payment
    
    * added strip payment of credit card feature completed
    
    * removed unwanted console
    
    ---------
    
    Co-authored-by: Aftab Alam <aftabf8982@gmail.com>

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -15,12 +15,10 @@ const PaymentForm = () => {
 
   const paymentHandler = async (e) => {
     e.preventDefault();
-
     if (!stripe || !elements) {
       return;
     }
     setIsProcessingPayment(true);
-    console.log(currentUser);
     const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "POST",
       headers: {
@@ -28,8 +26,33 @@ const PaymentForm = () => {
       },
       body: JSON.stringify({ amount: 100 * totalAmount }),
     }).then((res) => res.json());
-    console.log(response);
-
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: currentUser ? currentUser && currentUser.diaplayName : "Guest",
+          address: {
+            line1: "flate 25 Dhaka",
+            line2: "bihar india",
+            postal_code: 845418,
+            city: "Dhaka",
+            state: "BR",
+            country: "IN",
+          },
+        },
+      },
+    });
+    setIsProcessingPayment(false);
+    if (paymentResult.error) {
+      alert(paymentResult.error.message);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("payment sucessful");
+      }
+    }
 
     const {
       paymentIntent: { client_secret },
@@ -69,7 +92,6 @@ const PaymentForm = () => {
       <div className="for-container">
         <h2>Cradit card payment: </h2>
         <CardElement />
-
         <Button
           isLoading={isProcessingPayment}
           style={{ marginTop: "20px" }}
--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,3 +1,4 @@
 import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
 
 export const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLICABLE_KEY}`);
